855ddad00e05d38dc6ffa0837765d213204404f4
jack <jmeixensperger@gmail.com>
2018-02-09 17:12:10 -0800

    second commit

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    private Transform mypos;
+    private Transform camerapos;
+    public GameObject camera;
+    public Particle sabercollision;
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+        camerapos = camera.transform;
+        mypos = this.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var moveAmount = speed * Time.deltaTime;
+        mypos.position = Vector3.MoveTowards(mypos.position, camerapos.position, speed);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Main Camera")
+            Destroy(this);
+    }
+
+}
