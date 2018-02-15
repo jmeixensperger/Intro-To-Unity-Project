41ab8f3826cac18c26a7f27c48c1080a56d1445a
jmeixensperger <jmeixensperger@gmail.com>
2018-02-15 15:00:26 -0800

    made prefabs and added score + behavior effects

--- Assets/Scripts/ShooterController.cs
+++ Assets/Scripts/ShooterController.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 
 public class ShooterController : MonoBehaviour {
 
+    public UIController gui;
     public Shooter[] shooters;
     public float delayTime;
     private float timeTaken;
     private bool active;
+    private int score;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         active = false;
+        score = 0;
     }
-	
+
 	// Update is called once per frame
 	void Update () {
         // check delay time for next bullet's fire time
@@ -29,7 +32,23 @@ public class ShooterController : MonoBehaviour {
             }
             timeTaken = 0;
         }
+        // re-calculate delayTime based on score (increment every 5 blocks)
+        if (score >= 5)
+        {
+            delayTime = delayTime * 0.75f;
+            Debug.Log("delayTime reduced to: " + delayTime.ToString());
+            score = 0;
+        }
 	}
+
+    public void incrementScore()
+    {
+        score++;
+    }
+
+    public bool isActive() {
+        return active;
+    }
 
     // Bullets will only fire when the controller is "active"
     public void StartShooting() {
---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIController : MonoBehaviour
+{
+
+    public GameObject[] lifeSquares;
+    public ShooterController shooterController;
+    public Material red;
+    private int health;
+    private int score;
+
+    void Start()
+    {
+        health = 0;
+        score = 0;
+    }
+
+    private void OnGUI()
+    {
+        GUI.Box(new Rect(Screen.width - 100, 0, 100, 50), "Score: "+score.ToString());
+    }
+
+    public int getScore()
+    {
+        return score;
+    }
+
+    public int getCurrentHealth()
+    {
+        return health;
+    }
+
+    public int getMaxHealth()
+    {
+        return lifeSquares.Length;
+    }
+
+    public void changeColor()
+    {
+        lifeSquares[health].GetComponent<Renderer>().material = red;
+        health += 1;
+        if (health == lifeSquares.Length)
+            shooterController.StopShooting();
+    }
+
+    public void incrementScore()
+    {
+        score++;
+        shooterController.incrementScore();
+    }
+}
